a3cf14fe3bd383cc7ae4adf437f859bceff274d1
Breau Axel <abreau@iutbayonne.univ-pau.fr>
2021-12-14 09:16:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+   <head>
+      <title>Bienvenue</title>
+   </head>
+   <body>
+      Php c'est cool
+    <?php
+    echo ('Bonjour ');
+    echo ('Bienvenue sur mon site');
+    ?>
+   </body>
+</html>
