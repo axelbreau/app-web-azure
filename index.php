92fb6c356f4e00b80376e88b523c003ad1185606
Breau Axel <abreau@iutbayonne.univ-pau.fr>
2021-12-14 15:16:50 +0100

    connect bdd commit

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
+<!DOCTYPE html>
 <html>
-   <head>
-      <title>Bienvenue</title>
-   </head>
-   <body>
-      Php c'est cool
-    <?php
-    echo ('Bonjour ');
-    echo ('Bienvenue sur mon site');
-    ?>
-   </body>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="cours.css">
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>  
+        <?php
+            $servername = 'bddmysql1.mysql.database.azure.com';
+            $username = 'axel';
+            $password = 'P@ssword';
+            
+            //On établit la connexion
+            $conn = new mysqli($servername, $username, $password);
+            
+            //On vérifie la connexion
+            if($conn->connect_error){
+                die('Erreur : ' .$conn->connect_error);
+            }
+            echo 'Connexion réussie';
+        ?>
+    </body>
 </html>
