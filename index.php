c27d3e9109b582400963a12041e19fd964312dc6
Breau Axel <abreau@iutbayonne.univ-pau.fr>
2021-12-14 16:37:30 +0100

    test

--- index.php
+++ index.php
@@ -1,31 +1,29 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Cours PHP / MySQL</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="cours.css">
-    </head>
-    <body>
-        <h1>Bases de données MySQL</h1>  
-        <?php
-            $servername = 'bddmysql1.mysql.database.azure.com';
-            $username = 'axel';
-            $password = 'P@ssword';
-            
-            //On établit la connexion
-            $conn = new mysqli($servername, $username, $password);
-            
-            //On vérifie la connexion
-            if($conn->connect_error){
-                die('Erreur : ' .$conn->connect_error);
-            }
-            echo 'Connexion réussie';
-            echo 'Test';
-            
-            //On prépare notre requête 
-            $sql = $conn->query("select * from utilisateur");
-            var_dump($sql);
-            print_r($sql);
-        ?>
-    </body>
-</html>
+<?php
+   try
+   {
+      // On se connecte à MySQL
+      $mysqlClient = new PDO('mysql:host=bddmysql1.mysql.database.azure.com;dbname=mabdd;charset=utf8', 'axel', 'P@ssword');
+   }
+   catch(Exception $e)
+   {
+      // En cas d'erreur, on affiche un message et on arrête tout
+      die('Erreur : '.$e->getMessage());
+   }
+
+   // Si tout va bien, on peut continuer
+
+   // On récupère tout le contenu de la table utilisateur
+   $sqlQuery = 'SELECT * FROM utilisateur';
+   $userStatement = $mysqlClient->prepare($sqlQuery);
+   $userStatement->execute();
+   $user = $userStatement->fetchAll();
+
+   // On affiche chaque recette une à une
+   foreach ($user as $recipe) {
+      <p>
+         <?php echo $recipe['author']; ?>
+      </p>
+   <?php
+   }
+   ?>
+?>
